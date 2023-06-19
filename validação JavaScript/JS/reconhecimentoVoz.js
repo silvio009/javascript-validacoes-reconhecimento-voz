4ff98c9f71d4a576ac3785af62d69f3b6237f95c
silvio009 <rm550821@fiap.com.br>
2023-06-19 16:19:46 -0300

    fazendo a página entender a voz

--- validação JavaScript/JS/reconhecimentoVoz.js
+++ validação JavaScript/JS/reconhecimentoVoz.js
@@ -1,4 +1,4 @@
-
+const elementoChute = document.getElementById('chute')
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;     // modulo do chrome para enetender o reconhecimento de voz
 const recognition = new SpeechRecognition();
 
@@ -6,7 +6,14 @@ recognition.lang = 'pt-br'
 recognition.start();
 
 recognition.addEventListener('result', onSpeak )
-
+//O innerHTML é uma propriedade do Element que define ou retorna o conteúdo HTML de um elemento
+function exibirChute(chute){
+    elementoChute.innerHTML = `            
+    <div>você disse:</div>
+    <span class="box">${chute}</span>
+    `
+}
 function onSpeak(evento){
- console.log(evento.results[0] [0].transcript)                // puxar somente o reconhecimento de voz 
+    chute = evento.results[0] [0].transcript              // puxar somente o reconhecimento de voz 
+    exibirChute(chute)
 }
